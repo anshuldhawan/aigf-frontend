575ea42a39a736eae84f890bf23b6ad1327e14bd
Abhidutt <abhidutt.sd@suffescom.com>
2023-08-08 12:07:35 +0530

    logout button in header

--- src/Layout/AdminHeader.jsx
+++ src/Layout/AdminHeader.jsx
@@ -12,7 +12,7 @@ export const AdminHeader = () => {
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">
+              <Dropdown.Item href="/admin/login">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="14"
